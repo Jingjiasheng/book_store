d12c86805f31877aad84a669d75b1b5bb4dccf70
hanchao <hanchao@sina.com>
2020-12-05 20:47:06 +0800

    添加三个模块的动态代理AsoectJ配置类，顶不顶用还未验证！补上开启代理的注解@EnableAspectJAutoProxy

--- order-manager/src/main/java/com/asheng/book_store/OrderManagerApplication.java
+++ order-manager/src/main/java/com/asheng/book_store/OrderManagerApplication.java
@@ -4,10 +4,12 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author 胜&SS
  */
+@EnableAspectJAutoProxy
 @EnableSwagger2Doc
 @SpringBootApplication
 @EnableEurekaClient
--- user-manager/src/main/java/com/asheng/book_store/UserManagerApplication.java
+++ user-manager/src/main/java/com/asheng/book_store/UserManagerApplication.java
@@ -4,7 +4,9 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 @EnableSwagger2Doc
 @EnableEurekaClient
