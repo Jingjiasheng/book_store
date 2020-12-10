6b4e0c35428a508b93f3c814bb21b5d042de18c6
hanchao <hanchao@sina.com>
2020-12-10 17:13:44 +0800

    将图书状态与订单状态定制到实体类中生成静态类成员常量以便以直接调用

--- book-manager/src/main/java/com/asheng/book_store/domain/BookState.java
+++ book-manager/src/main/java/com/asheng/book_store/domain/BookState.java
@@ -9,6 +9,19 @@ import java.io.Serializable;
  * @since 2020-12-02 14:32:56
  */
 public class BookState implements Serializable {
+    //未发布
+    private static final long NOT_PUBLISH = 201;
+    //正在发布，暂未购买
+    private static final long PUBLISHING = 202;
+    //正在交易
+    private static final long TRANSACTION = 203;
+    //被购买
+    private static final long PURCHASED = 204;
+    //已下架
+    private static final long OFF_THE_SHELF = 205;
+    //被删除
+    private static final long DELETED = 206;
+
     private static final long serialVersionUID = 773774195941241659L;
     /**
      * 书籍在平台中存放的状态ID
--- order-manager/src/main/java/com/asheng/book_store/domain/OrderState.java
+++ order-manager/src/main/java/com/asheng/book_store/domain/OrderState.java
@@ -10,7 +10,27 @@ import java.io.Serializable;
  */
 public class OrderState implements Serializable {
     private static final long serialVersionUID = 647471477067491263L;
-    
+    //发布中
+    private static final long ORDER_PUBLISHING = 647471477067491263L;
+    //被下单
+    private static final long ORDER_PLACE = 647471477067491263L;
+    //支付中
+    private static final long ORDER_PAYING = 647471477067491263L;
+    //支付失败
+    private static final long ORDER_PAY_FAIL = 647471477067491263L;
+    //支付成功
+    private static final long OEDER_PAY_SUCCESS = 647471477067491263L;
+    //已发货
+    private static final long ORDER_GOODS_DELIVERED = 647471477067491263L;
+    //货物异常
+    private static final long ORDER_GOODS_ABNORMAL = 647471477067491263L;
+    //已收货
+    private static final long ORDER_GOOD_RECEIVIED = 647471477067491263L;
+    //订单正常结束
+    private static final long ORDER_END_NORMAL = 647471477067491263L;
+    //订单异常结束
+    private static final long ORDER_END_ABNORMAL = 647471477067491263L;
+
     private Integer orderStateId;
     
     private String orderStateExplain;
